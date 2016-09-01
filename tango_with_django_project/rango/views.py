28696037fe4d6d7ba6dfaa04665c3755238da763
anzhelaGu <2144620i@student.gla.ac.uk>
2016-09-01 14:41:18 +0100

    Added page_list to the index view

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -13,7 +13,11 @@ def index(request):
     # Retrieve the top 5 only - or all if less than 5.
     # Place the list in our context_dict dictionary which will be passed to the template engine.
     category_list = Category.objects.order_by('-likes')[:5]
-    context_dict = {'categories': category_list}
+
+    #Top 5 most viewed pages
+    page_list = Page.objects.order_by('-views')[:5]
+    
+    context_dict = {'categories': category_list, 'pages' : page_list}
 
     # Render the response and send it back!
     return render(request, 'rango/index.html', context_dict)
