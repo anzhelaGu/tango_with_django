c7225e650af0820833cf5464661644c62e06abee
anzhelaGu <2144620i@student.gla.ac.uk>
2016-09-09 17:39:53 +0100

    Added an "Add" button next to each search result

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -306,3 +306,24 @@ def suggest_category(request):
         cat_list = get_category_list(8, starts_with)
 
         return render(request, 'rango/cats.html', {'cat_list': cat_list })
+
+@login_required
+def auto_add_page(request):
+    cat_id = None
+    url = None
+    title = None
+    context_dict = {}
+    if request.method == 'GET':
+        cat_id = request.GET['category_id']
+        url = request.GET['url']
+        title = request.GET['title']
+        if cat_id:
+            category = Category.objects.get(id=int(cat_id))
+            p = Page.objects.get_or_create(category=category, title=title, url=url)
+
+            pages = Page.objects.filter(category=category).order_by('-views')
+
+            # Adds our results list to the template context under name pages.
+            context_dict['pages'] = pages
+
+    return render(request, 'rango/category.html', context_dict)
